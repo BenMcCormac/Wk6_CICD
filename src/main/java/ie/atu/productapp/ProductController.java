d59b9ac83b32ce3f2179e85cccf63defb6ad4ccc
G00416547@atu.ie <G00416547@atu.ie>
2024-10-22 10:55:52 +0100

    createProduct works

--- src/main/java/ie/atu/productapp/ProductController.java
+++ src/main/java/ie/atu/productapp/ProductController.java
@@ -1,5 +1,6 @@
 package ie.atu.productapp;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,7 @@ public class ProductController
     private List<Product> list = new ArrayList<>();
     //Creating a class interested in requests and responses,  Separation of Concern
 
-
+    @PostMapping("/createProduct")
     public List<Product> newProduct(@RequestBody Product product)
     {
         //send it to business logic
